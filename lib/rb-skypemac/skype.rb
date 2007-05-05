5eb0125667d4e1426c151f2a441d5a2d1bb6695b
sleight42 <sleight42@c1a9f598-a9ef-4ba3-b554-1ed72fcd1b56>
2007-05-05 15:38:12 +0000

    Rdoc fixes
    git-svn-id: http://rb-skypemac.rubyforge.org/svn@16 c1a9f598-a9ef-4ba3-b554-1ed72fcd1b56

--- lib/rb-skypemac/skype.rb
+++ lib/rb-skypemac/skype.rb
@@ -28,13 +28,13 @@ module SkypeMac
       call
     end
 
-    # Returns an Array of call IDs if there is an incoming Skype call otherwise nil
+    # Returns an Array of Call IDs if there is an incoming Skype call otherwise nil
     def Skype.incoming_calls
       calls = Call.active_calls - @@calls
       calls
     end
     
-    # Answers a call given a skype call ID.  Returns an Array of Call objects.
+    # Answers a call given a skype Call ID.  Returns an Array of Call objects.
     def Skype.answer(call)
       cmd = "ALTER CALL #{call.call_id} ANSWER"
       r = Skype.send_ :command => cmd
@@ -54,7 +54,7 @@ module SkypeMac
       @@groups
     end
     
-    # Returns Array of all users in Group.  Accepts types as defined by Group.types
+    # Returns Array of all User in a particular Group type.  Accepts types as defined by Group.types
     def Skype.find_users_of_type(group_type)
       begin
         Skype.groups.find { |g| g.gtype == group_type}.users
@@ -68,32 +68,32 @@ module SkypeMac
       Skype.find_users_of_type "ONLINE_FRIENDS"
     end
     
-    # Array of all Users that are friends of the current user
+    # Array of all User that are friends of the current user
     def Skype.all_friends
       Skype.find_users_of_type "ALL_FRIENDS"
     end
     
-    # Array of all Users defined as Skype Out users
+    # Array of all User defined as Skype Out users
     def Skype.skypeout_friends
       Skype.find_users_of_type "SKYPEOUT_FRIENDS"
     end
     
-    # Array of all Users that the user knows
+    # Array of all User that the user knows
     def Skype.all_users
       Skype.find_users_of_type "ALL_USERS"
     end
     
-    # Array of Users recently contacted by the user, friends or not
+    # Array of User recently contacted by the user, friends or not
     def Skype.recently_contacted_users
       Skype.find_users_of_type "RECENTLY_CONTACTED_USERS"
     end
     
-    # Array of Users waiting for authorization
+    # Array of User waiting for authorization
     def Skype.users_waiting_for_authorization
       Skype.find_users_of_type "USERS_WAITING_MY_AUTHORIZATION"
     end
     
-    # Array of Users blocked
+    # Array of User blocked
     def Skype.blocked_users
       Skype.find_users_of_type "USERS_BLOCKED_BY_ME"
     end
